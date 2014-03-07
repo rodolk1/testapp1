e3cd0be8d35830ee13837fc3d92b696fe7f73efc
rodolk1 <rodolk@localhost.localdomain>
2014-03-07 03:02:01 -0500

    added uglify and jshint

--- Gruntfile.js
+++ Gruntfile.js
@@ -8,18 +8,33 @@ module.exports = function(grunt) {
         banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
       },
       build: {
-        src: '<%= pkg.name %>.js',
-        dest: 'build/<%= pkg.name %>.min.js'
+        files: {
+          src: ['app.js', 'routes/**/*.js'],
+          dest: 'build/js'
+        }
+      }
+    },
+    jshint: {
+      files: ['Gruntfile.js', 'app.js', 'routes/**/*.js'],
+      options: {
+        // options here to override JSHint defaults
+        globals: {
+          jQuery: true,
+          console: true,
+          module: true,
+          document: true
+        }
       }
     }
   });
 
   // Load the plugin that provides the "uglify" task.
   grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
 
   // Default task(s).
   grunt.registerTask('default', ['uglify']);
-
+  grunt.registerTask('test', ['jshint']);
 };
 
 
