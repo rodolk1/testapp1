bc2e5bd3bc3b3426e7a1878206d3e1f2b7cd2c2d
rodolk1 <rodolk@localhost.localdomain>
2014-03-07 11:24:20 -0500

    notifications enabled jshint fixed

--- routes/user.js
+++ routes/user.js
@@ -6,7 +6,7 @@ exports.userlist = function(db) {
   return function(req, res) {
     db.collection('userlist').find().toArray(function (err, items) {
       res.json(items);
-    })
-  }
+    });
+  };
 };
 
